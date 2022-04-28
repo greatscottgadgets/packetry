3571beb79cff6469d1bb3fe36b2807b08b99602a
Martin Ling <martin-git@earth.li>
2022-04-28 06:07:03 +0100

    Separate child_item from get_item which takes an Option.

--- src/capture.rs
+++ src/capture.rs
@@ -1317,11 +1317,10 @@ impl Capture {
         self.endpoint_state_index.push(state_offset).unwrap();
     }
 
-    pub fn get_item(&mut self, parent: &Option<Item>, index: u64) -> Item {
+    fn child_item(&mut self, parent: &Item, index: u64) -> Item {
         use Item::*;
         match parent {
-            None => Transfer(self.item_index.get(index).unwrap()),
-            Some(Transfer(transfer_index_id)) =>
+            Transfer(transfer_index_id) =>
                 Transaction(*transfer_index_id, {
                     let entry = self.transfer_index.get(*transfer_index_id).unwrap();
                     let endpoint_id = entry.endpoint_id() as usize;
@@ -1330,10 +1329,18 @@ impl Capture {
                     let offset = ep_data.transfer_index.get(transfer_id).unwrap();
                     ep_data.transaction_ids.get(offset + index).unwrap()
                 }),
-            Some(Transaction(transfer_index_id, transaction_id)) =>
-                Packet(*transfer_index_id, *transaction_id, {
-                    self.transaction_index.get(*transaction_id).unwrap() + index}),
-            Some(Packet(..)) => panic!("packets do not have children"),
+            Transaction(transfer_index_id, transaction_id) =>
+                Packet(*transfer_index_id, *transaction_id,
+                       self.transaction_index.get(*transaction_id).unwrap() + index),
+            Packet(..) => panic!("packets do not have children")
+        }
+    }
+
+    pub fn get_item(&mut self, parent: &Option<Item>, index: u64) -> Item {
+        use Item::*;
+        match parent {
+            None => Transfer(self.item_index.get(index).unwrap()),
+            Some(item) => self.child_item(item, index)
         }
     }
 
