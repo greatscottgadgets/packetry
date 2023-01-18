60fd9a48b1dcd22fb15305b2dbcfb593c00b2081
Martin Ling <martin-git@earth.li>
2023-01-18 16:08:19 +0000

    Add row_count method.

--- src/tree_list_model.rs
+++ src/tree_list_model.rs
@@ -390,6 +390,10 @@ where Item: 'static + Copy,
         })
     }
 
+    fn row_count(&self) -> u32 {
+        self.root.borrow().children().total_count
+    }
+
     pub fn set_expanded(&self,
                         model: &Model,
                         node_ref: &ItemNodeRc<Item>,
@@ -857,13 +861,13 @@ where Item: 'static + Copy,
     // called by a GObject wrapper class to implement that interface.
 
     pub fn n_items(&self) -> u32 {
-        self.root.borrow().children.total_count
+        self.row_count()
     }
 
     pub fn item(&self, position: u32) -> Option<Object> {
         // First check that the position is valid (must be within the root
         // node's total child count).
-        if position >= self.root.borrow().children.total_count {
+        if position >= self.row_count() {
             return None
         }
         let node_or_err_msg = self.fetch(position).map_err(|e| format!("{e:?}"));
