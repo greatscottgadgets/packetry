4456ce930bee7ae4757f75d0966dc0a2e514045b
straithe <b@straithe.com>
2023-10-30 17:19:48 -0400

    Add initial Packetry documentation kit

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+import sphinx_rtd_theme
+
+extensions = [
+    'sphinx_rtd_theme'
+]
+
+# -- Project information -----------------------------------------------------
+
+project = 'Packetry'
+copyright = '2023, Great Scott Gadgets'
+author = 'Great Scott Gadgets'
+
+version = ''
+release = ''
+
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+  'sphinx.ext.autodoc'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = []
+pygments_style = None
+
+
+# -- Options for HTML output -------------------------------------------------
+# run pip install sphinx_rtd_theme if you get sphinx_rtd_theme errors
+html_theme = "sphinx_rtd_theme"
+html_css_files = ['status.css']
