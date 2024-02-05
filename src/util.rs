d3effd958c2afa50ae2406a92afbd2c6de237e0f
Martin Ling <martin-git@earth.li>
2024-02-05 14:31:23 +0000

    Update humansize dependency to current v2.1.3 release.

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 use num_format::{Locale, ToFormattedString};
-use humansize::{FileSize, file_size_opts as options};
+use humansize::{SizeFormatter, BINARY};
 
 pub fn fmt_count(count: u64) -> String {
     count.to_formatted_string(&Locale::en)
@@ -11,9 +11,6 @@ pub fn fmt_size(size: u64) -> String {
     } else if size < 1024 {
         format!("{size} bytes")
     } else {
-        match size.file_size(options::BINARY) {
-            Ok(string) => string,
-            Err(e) => format!("<Error: {e}>")
-        }
+        format!("{}", SizeFormatter::new(size, BINARY))
     }
 }
