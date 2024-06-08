74eca56d8a6205c148fe1f14f5b04e9c5b259341
Martin Ling <martin-git@earth.li>
2024-06-08 21:15:47 +0100

    Add a helper function to handle thread panics.

--- src/backend/cynthion.rs
+++ src/backend/cynthion.rs
@@ -424,19 +424,24 @@ impl CynthionStop {
         println!("Requesting capture stop");
         self.stop_request.send(())
             .or_else(|_| bail!("Failed sending stop request"))?;
-        match self.worker.join() {
-            Ok(()) => Ok(()),
-            Err(panic) => {
-                let msg = match (
-                    panic.downcast_ref::<&str>(),
-                    panic.downcast_ref::<String>())
-                {
-                    (Some(&s), _) => s,
-                    (_,  Some(s)) => s,
-                    (None,  None) => "<No panic message>"
-                };
-                bail!("Worker thread panic: {msg}");
-            }
+        handle_thread_panic(self.worker.join())?;
+        Ok(())
+    }
+}
+
+fn handle_thread_panic<T>(result: std::thread::Result<T>) -> Result<T, Error> {
+    match result {
+        Ok(x) => Ok(x),
+        Err(panic) => {
+            let msg = match (
+                panic.downcast_ref::<&str>(),
+                panic.downcast_ref::<String>())
+            {
+                (Some(&s), _) => s,
+                (_,  Some(s)) => s,
+                (None,  None) => "<No panic message>"
+            };
+            bail!("Worker thread panic: {msg}");
         }
     }
 }
