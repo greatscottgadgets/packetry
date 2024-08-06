79b6b5b91b47d98d20da68f01379bab29ab54038
manu <manuel.kiesel@cyllective.com>
2024-08-06 22:13:36 +0200

    moved cli capturing to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -8,36 +8,277 @@ const MAIN_BINARY: &str = "packetry.exe";
 #[cfg(not(windows))]
 const MAIN_BINARY: &str = "packetry";
 
+// We need the bitfield macro.
+#[macro_use]
+extern crate bitfield;
+
+mod backend;
+mod pcap;
+
+use crate::backend::cynthion::{CynthionDevice, CynthionUsability::*, Speed};
+
+use crate::pcap::Writer;
+
+use anyhow::Error;
+use std::fs::File;
+use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
+use tabled::{Table, Tabled};
+
+use argh::FromArgs;
+#[derive(FromArgs, PartialEq, Debug)]
+/// packetry - a fast, intuitive USB 2.0 protocol analysis application for use with Cynthion
+struct Args {
+    #[argh(subcommand)]
+    sub_commands: Option<SubcommandEnum>,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum SubcommandEnum {
+    One(SubCommandOpenFileInGui),
+    Two(SubCommandCliCapture),
+    Three(SubCommandDevices),
+}
+#[derive(FromArgs, PartialEq, Debug)]
+/// Opens a recording in the GUI
+#[argh(subcommand, name = "open")]
+struct SubCommandOpenFileInGui {
+    /// recording (pcap) to open in the GUI
+    #[argh(positional)]
+    filename: String,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// Start packetry in CLI mode
+#[argh(subcommand, name = "capture")]
+struct SubCommandCliCapture {
+    /// device (default: auto)
+    #[argh(
+        option,
+        short = 'd',
+        long = "device",
+        default = "String::from(\"auto\")"
+    )]
+    device_serial: String,
+    /// usb speed (default: low)
+    #[argh(option, short = 's', long = "speed", default = "Speed::Low")]
+    usb_speed: Speed,
+    /// output file (use '-' for stdout, default: cynthion.pcap)
+    #[argh(
+        option,
+        short = 'o',
+        long = "output",
+        default = "String::from(\"cynthion.pcap\")"
+    )]
+    output_file: String,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// List capture devices
+#[argh(subcommand, name = "devices")]
+struct SubCommandDevices {}
+
+#[derive(Tabled)]
+pub struct DeviceInfo {
+    name: String,
+    serial: String,
+    useable: String,
+    bus: String,
+    address: String,
+    speeds: String,
+}
+
 fn main() -> ExitCode {
-    // Find the main Packetry executable.
-    let packetry_binary_path = std::env::current_exe()
-        .expect("Failed to find path to current executable")
-        .parent()
-        .expect("Failed to find parent directory of current executable")
-        .join(MAIN_BINARY);
+    let args: Args = argh::from_env();
 
-    // Prepare to call it, passing through all arguments we were passed.
-    let mut command = Command::new(packetry_binary_path);
-    command.args(std::env::args().skip(1));
-    
-    // If on Windows, tell the child that it needs to attach to our console.
-    #[cfg(windows)]
-    command.env("PACKETRY_ATTACH_CONSOLE", "1");
+    let exit_code = if let Some(subcmd) = args.sub_commands {
+        match subcmd {
+            SubcommandEnum::One(file) => {
+                // Find the main Packetry executable.
+                let packetry_binary_path = std::env::current_exe()
+                    .expect("Failed to find path to current executable")
+                    .parent()
+                    .expect("Failed to find parent directory of current executable")
+                    .join(MAIN_BINARY);
 
-    // Spawn the main binary as a child process, and wait for its exit status.
-    let exit_status = command 
-        .status()
-        .expect("Failed to start main packetry binary");
-    
-    // Try to exit with the same code the child did.
-    match exit_status.code() {
-        Some(code) => ExitCode::from(code as u8),
-        None => {
-            #[cfg(unix)]
-            if let Some(signal) = exit_status.signal() {
-                panic!("Packetry was terminated by signal {signal}");
+                // Prepare to call it, passing through all arguments we were passed.
+                let mut command = Command::new(packetry_binary_path);
+                command.args(&[&file.filename]);
+
+                // If on Windows, tell the child that it needs to attach to our console.
+                #[cfg(windows)]
+                command.env("PACKETRY_ATTACH_CONSOLE", "1");
+
+                // Spawn the main binary as a child process, and wait for its exit status.
+                let exit_status = command
+                    .status()
+                    .expect("Failed to start main packetry binary");
+
+                // Try to exit with the same code the child did.
+                match exit_status.code() {
+                    Some(code) => ExitCode::from(code as u8),
+                    None => {
+                        #[cfg(unix)]
+                        if let Some(signal) = exit_status.signal() {
+                            panic!("Packetry was terminated by signal {signal}");
+                        }
+                        panic!("Packetry was terminated without an exit code");
+                    }
+                }
             }
-            panic!("Packetry was terminated without an exit code");
+
+            SubcommandEnum::Two(captureoptions) => {
+                if let Err(e) = headless_capture(captureoptions) {
+                    eprintln!("Error capturing packets: {}", e);
+                    ExitCode::FAILURE
+                } else {
+                    ExitCode::SUCCESS
+                }
+            }
+
+            SubcommandEnum::Three(_) => {
+                if let Err(e) = list_devices() {
+                    eprintln!("Error listing devices: {}", e);
+                    ExitCode::FAILURE
+                } else {
+                    ExitCode::SUCCESS
+                }
+            }
+        }
+    } else {
+        ExitCode::SUCCESS
+    };
+
+    exit_code
+}
+
+fn headless_capture(options: SubCommandCliCapture) -> Result<(), Error> {
+    let device = select_device(&options.device_serial, options.usb_speed)?;
+
+    let output_writer: Box<dyn Write> = if options.output_file == "-" {
+        Box::new(io::stdout())
+    } else {
+        Box::new(File::create(&options.output_file)?)
+    };
+    let mut writer = Writer::open(output_writer)?;
+
+    let cynthion = device.open()?;
+    let (stream_handle, stop_handle) =
+        cynthion.start(options.usb_speed, |e| eprintln!("{:?}", e))?;
+
+    let stop_handle = Arc::new(Mutex::new(Some(stop_handle)));
+    ctrlc::set_handler(move || {
+        let mut handle_opt = stop_handle.lock().unwrap();
+        if let Some(handle) = handle_opt.take() {
+            if let Err(e) = handle.stop() {
+                eprintln!("Failed to stop Cynthion: {}", e);
+            }
+        }
+    })
+    .expect("Error setting CTRL+C handler");
+
+    for packet in stream_handle {
+        writer.add_packet(&packet.bytes, packet.timestamp_ns)?;
+    }
+
+    writer.close()?;
+
+    Ok(())
+}
+
+fn select_device(serial: &str, speed: Speed) -> Result<CynthionDevice, Error> {
+    let mut devices = CynthionDevice::scan()?;
+
+    if devices.is_empty() {
+        return Err(anyhow::anyhow!("No devices found"));
+    }
+
+    let device_index = if serial == "auto" {
+        // Select the first device
+        Some(0)
+    } else {
+        // Find the device with the requested serial number
+        devices
+            .iter()
+            .position(|d| d.device_info.serial_number() == Some(&serial.to_string()))
+    };
+
+    // Check if the device was found
+    let device_index = match device_index {
+        Some(index) => index,
+        None => return Err(anyhow::anyhow!("Device with serial {} not found", serial)),
+    };
+
+    // Remove the device from the list to get ownership
+    let device = devices.remove(device_index);
+
+    // Check if the device is usable and supports the requested speed
+    match &device.usability {
+        Usable(_, speeds) => {
+            if !speeds.contains(&speed) {
+                return Err(anyhow::anyhow!("Device does not support speed {:?}", speed));
+            }
+        }
+
+        Unusable(reason) => {
+            return Err(anyhow::anyhow!("Device is not usable: {}", reason));
         }
     }
+
+    Ok(device)
+}
+
+pub fn list_devices() -> Result<(), Error> {
+    let devices = CynthionDevice::scan()?;
+
+    if devices.is_empty() {
+        println!("No devices found.");
+        return Ok(());
+    }
+
+    let device_table: Vec<DeviceInfo> = devices
+        .iter()
+        .map(|device| {
+            let info = &device.device_info;
+
+            // Maybe in the future there are more devices to support. Hardcode the name for now.
+            let name = "Cynthion".to_string();
+
+            let serial = info
+                .serial_number()
+                .map_or("None".to_string(), |s| s.to_string());
+            let bus = info.bus_number().to_string();
+            let address = info.device_address().to_string();
+
+            let (useable, speeds) = match &device.usability {
+                Usable(_, speeds) => (
+                    "Yes".to_string(),
+                    speeds
+                        .iter()
+                        .map(|speed| {
+                            let desc = speed.description();
+                            desc.split(' ').next().unwrap_or("").to_string()
+                        })
+                        .collect::<Vec<String>>()
+                        .join(", "),
+                ),
+                Unusable(reason) => (reason.to_string(), String::new()),
+            };
+
+            DeviceInfo {
+                name,
+                serial,
+                useable,
+                bus,
+                address,
+                speeds,
+            }
+        })
+        .collect();
+
+    let table = Table::new(device_table).to_string();
+    println!("{}", table);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,23 @@
 extern crate bitfield;
 
 // We need the ctor macro for the replay test on macOS.
-#[cfg(all(test, target_os = "macos"))]
+#[cfg(all(test, target_os="macos"))]
 #[allow(unused_imports)]
 #[macro_use]
 extern crate ctor;
 
 // Include build-time info.
 pub mod built {
-    // The file has been placed there by the build script.
-    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+   // The file has been placed there by the build script.
+   include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
 // Declare all modules used.
 mod backend;
 mod capture;
-mod cli_capture;
 mod compact_index;
 mod data_stream;
 mod decoder;
-mod devices;
 mod expander;
 mod id;
 mod index_stream;
@@ -42,61 +40,25 @@ mod vec_map;
 mod version;
 
 // Declare optional modules.
-#[cfg(any(test, feature = "record-ui-test"))]
+#[cfg(any(test, feature="record-ui-test"))]
 mod record_ui;
 #[cfg(test)]
 mod test_replay;
 
-use gtk::gio::ApplicationFlags;
 use gtk::prelude::*;
+use gtk::gio::ApplicationFlags;
+use gtk::glib::{self, OptionArg, OptionFlags};
 
-use cli_capture::{headless_capture, SubCommandCliCapture};
-use devices::list_devices;
-use ui::{activate, display_error, open, stop_cynthion};
+use ui::{
+    activate,
+    display_error,
+    open,
+    stop_cynthion
+};
 use version::{version, version_info};
 
-use argh::FromArgs;
-#[derive(FromArgs, PartialEq, Debug)]
-/// packetry - a fast, intuitive USB 2.0 protocol analysis application for use with Cynthion
-struct Args {
-    #[argh(subcommand)]
-    sub_commands: Option<SubcommandEnum>,
-
-    /// recording (pcap) to open in the GUI
-    #[argh(positional)]
-    filename: Option<String>,
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand)]
-enum SubcommandEnum {
-    One(SubCommandCliCapture),
-    Two(SubCommandDevices),
-    Three(SubCommandVersion),
-    Four(SubCommandTest),
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// List capture devices
-#[argh(subcommand, name = "devices")]
-struct SubCommandDevices {}
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// Print version information
-#[argh(subcommand, name = "version")]
-struct SubCommandVersion {
-    /// print dependency information with version
-    #[argh(switch, long = "dependencies")]
-    print_dependencies: bool,
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// Test an attached Cynthion
-#[argh(subcommand, name = "test-cynthion")]
-struct SubCommandTest {
-    /// save the captures to disk
-    #[argh(switch, long = "save-captures")]
-    save_captures: bool,
+fn have_argument(name: &str) -> bool {
+    std::env::args().any(|arg| arg == name)
 }
 
 fn main() {
@@ -107,49 +69,35 @@ fn main() {
         unsafe { AttachConsole(ATTACH_PARENT_PROCESS) };
     }
 
-    let args: Args = argh::from_env();
-
-    if let Some(subcmd) = args.sub_commands {
-        match subcmd {
-            SubcommandEnum::One(captureoptions) => {
-                if let Err(e) = headless_capture(captureoptions) {
-                    eprintln!("Error capturing: {}", e);
-                }
-            }
-
-            SubcommandEnum::Two(_) => {
-                if let Err(e) = list_devices() {
-                    eprintln!("Error listing devices: {}", e);
-                }
-            }
-
-            SubcommandEnum::Three(versionoptions) => {
-                println!(
-                    "Packetry version {}\n\n{}",
-                    version(),
-                    version_info(versionoptions.print_dependencies)
-                );
-            }
-
-            SubcommandEnum::Four(testoptions) => {
-                test_cynthion::run_test(testoptions.save_captures);
-            }
-        }
+    if have_argument("--version") {
+        println!("Packetry version {}\n\n{}",
+                 version(),
+                 version_info(have_argument("--dependencies")));
+    } else if have_argument("--test-cynthion") {
+        let save_captures = have_argument("--save-captures");
+        test_cynthion::run_test(save_captures);
     } else {
-        // Start the GUI application as no subcommand was provided.
-        // Here, args[1] is the filename if one was provided. This should be added to the argh enum.
-
         let application = gtk::Application::new(
             Some("com.greatscottgadgets.packetry"),
-            ApplicationFlags::NON_UNIQUE | ApplicationFlags::HANDLES_OPEN,
+            ApplicationFlags::NON_UNIQUE |
+            ApplicationFlags::HANDLES_OPEN
         );
-        application.set_option_context_parameter_string(Some("[filename.pcap]"));
+        application.set_option_context_parameter_string(
+           Some("[filename.pcap]"));
+        application.add_main_option(
+           "version", glib::Char::from(0),
+           OptionFlags::NONE, OptionArg::None,
+           "Print version information", None);
+        application.add_main_option(
+           "test-cynthion", glib::Char::from(0),
+           OptionFlags::NONE, OptionArg::None,
+           "Test an attached Cynthion USB analyzer", None);
         application.connect_activate(|app| display_error(activate(app)));
         application.connect_open(|app, files, _hint| {
-            app.activate();
-            if let Some(file) = files.first() {
-                display_error(open(file));
-            }
+           app.activate();
+           if let Some(file) = files.first() {
+              display_error(open(file));
+           }
         });
         application.run();
         display_error(stop_cynthion());
