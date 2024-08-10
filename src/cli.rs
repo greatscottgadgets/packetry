94bee135aaddf1573b3c4f0469eb9846f6de14ff
manu <manuel.kiesel@cyllective.com>
2024-08-10 22:09:34 +0200

    removed open option

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,5 @@
-use std::process::{Command, ExitCode};
+use std::process::ExitCode;
 
-#[cfg(unix)]
-use std::os::unix::process::ExitStatusExt;
-
-#[cfg(windows)]
-const MAIN_BINARY: &str = "packetry.exe";
-#[cfg(not(windows))]
-const MAIN_BINARY: &str = "packetry";
-
-// We need the bitfield macro.
 #[macro_use]
 extern crate bitfield;
 
@@ -36,17 +27,8 @@ struct Args {
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand)]
 enum SubcommandEnum {
-    One(SubCommandOpenFileInGui),
-    Two(SubCommandCliCapture),
-    Three(SubCommandDevices),
-}
-#[derive(FromArgs, PartialEq, Debug)]
-/// Opens a recording in the GUI
-#[argh(subcommand, name = "open")]
-struct SubCommandOpenFileInGui {
-    /// recording (pcap) to open in the GUI
-    #[argh(positional)]
-    filename: String,
+    One(SubCommandCliCapture),
+    Two(SubCommandDevices),
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
@@ -94,41 +76,7 @@ fn main() -> ExitCode {
 
     let exit_code = if let Some(subcmd) = args.sub_commands {
         match subcmd {
-            SubcommandEnum::One(file) => {
-                // Find the main Packetry executable.
-                let packetry_binary_path = std::env::current_exe()
-                    .expect("Failed to find path to current executable")
-                    .parent()
-                    .expect("Failed to find parent directory of current executable")
-                    .join(MAIN_BINARY);
-
-                // Prepare to call it, passing through all arguments we were passed.
-                let mut command = Command::new(packetry_binary_path);
-                command.args(&[&file.filename]);
-
-                // If on Windows, tell the child that it needs to attach to our console.
-                #[cfg(windows)]
-                command.env("PACKETRY_ATTACH_CONSOLE", "1");
-
-                // Spawn the main binary as a child process, and wait for its exit status.
-                let exit_status = command
-                    .status()
-                    .expect("Failed to start main packetry binary");
-
-                // Try to exit with the same code the child did.
-                match exit_status.code() {
-                    Some(code) => ExitCode::from(code as u8),
-                    None => {
-                        #[cfg(unix)]
-                        if let Some(signal) = exit_status.signal() {
-                            panic!("Packetry was terminated by signal {signal}");
-                        }
-                        panic!("Packetry was terminated without an exit code");
-                    }
-                }
-            }
-
-            SubcommandEnum::Two(captureoptions) => {
+            SubcommandEnum::One(captureoptions) => {
                 if let Err(e) = headless_capture(captureoptions) {
                     eprintln!("Error capturing packets: {}", e);
                     ExitCode::FAILURE
@@ -137,7 +85,7 @@ fn main() -> ExitCode {
                 }
             }
 
-            SubcommandEnum::Three(_) => {
+            SubcommandEnum::Two(_) => {
                 if let Err(e) = list_devices() {
                     eprintln!("Error listing devices: {}", e);
                     ExitCode::FAILURE
