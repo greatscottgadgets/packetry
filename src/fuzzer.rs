df054df8f1a1177a4b4007c63b4730f5090254d6
Martin Ling <martin-git@earth.li>
2024-08-27 15:49:35 +0100

    Add fuzzer.

---
+++ src/fuzzer.rs
@@ -0,0 +1,37 @@
+//! Fuzz the USB packet decoder.
+
+#![allow(dead_code)]
+#![no_main]
+
+#[macro_use]
+extern crate bitfield;
+
+use libfuzzer_sys::{arbitrary::{Arbitrary, Unstructured}, fuzz_target};
+
+mod capture;
+mod compact_index;
+mod data_stream;
+mod decoder;
+mod id;
+mod index_stream;
+mod pcap;
+mod rcu;
+mod stream;
+mod usb;
+mod util;
+mod vec_map;
+
+use capture::create_capture;
+use decoder::Decoder;
+
+fuzz_target!(|data: &[u8]| {
+    let mut input = Unstructured::new(data);
+    let packets = Vec::<(Vec::<u8>, u32)>::arbitrary(&mut input).unwrap();
+    let mut timestamp = u32::arbitrary(&mut input).unwrap() as u64;
+    let (writer, _reader) = create_capture().unwrap();
+    let mut decoder = Decoder::new(writer).unwrap();
+    for (packet, time_delta) in packets {
+        timestamp += time_delta as u64;
+        decoder.handle_raw_packet(&packet, timestamp).unwrap();
+    }
+});
