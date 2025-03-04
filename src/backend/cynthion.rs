a23e2a08cbc12d841896427681582ea304d643f9
Martin Ling <martin-git@earth.li>
2025-03-04 12:43:26 +0000

    Add support for non-packet events to backend API.

--- src/backend/cynthion.rs
+++ src/backend/cynthion.rs
@@ -22,9 +22,9 @@ use super::{
     BackendDevice,
     BackendHandle,
     Speed,
-    PacketIterator,
-    PacketResult,
-    TimestampedPacket,
+    EventIterator,
+    EventResult,
+    TimestampedEvent,
     TransferQueue,
 };
 
@@ -251,8 +251,8 @@ impl BackendHandle for CynthionHandle {
         Ok(())
     }
 
-    fn timestamped_packets(&self, data_rx: mpsc::Receiver<Vec<u8>>)
-        -> Box<dyn PacketIterator>
+    fn timestamped_events(&self, data_rx: mpsc::Receiver<Vec<u8>>)
+        -> Box<dyn EventIterator>
     {
         Box::new(
             CynthionStream {
@@ -330,16 +330,16 @@ impl CynthionHandle {
     }
 }
 
-impl PacketIterator for CynthionStream {}
+impl EventIterator for CynthionStream {}
 
 impl Iterator for CynthionStream {
-    type Item = PacketResult;
-    fn next(&mut self) -> Option<PacketResult> {
+    type Item = EventResult;
+    fn next(&mut self) -> Option<EventResult> {
         loop {
-            // Do we have another packet already in the buffer?
-            match self.next_buffered_packet() {
-                // Yes; return the packet.
-                Some(packet) => return Some(Ok(packet)),
+            // Do we have another event already in the buffer?
+            match self.next_buffered_event() {
+                // Yes; return the event.
+                Some(event) => return Some(Ok(event)),
                 // No; wait for more data from the capture thread.
                 None => match self.receiver.recv().ok() {
                     // Received more data; add it to the buffer and retry.
@@ -353,7 +353,9 @@ impl Iterator for CynthionStream {
 }
 
 impl CynthionStream {
-    fn next_buffered_packet(&mut self) -> Option<TimestampedPacket> {
+    fn next_buffered_event(&mut self) -> Option<TimestampedEvent> {
+        use TimestampedEvent::*;
+
         // Are we waiting for a padding byte?
         if self.padding_due {
             if self.buffer.is_empty() {
@@ -405,9 +407,9 @@ impl CynthionStream {
         }
 
         // Remove the rest of the packet from the buffer and return it.
-        Some(TimestampedPacket {
+        Some(Packet {
             timestamp_ns: clk_to_ns(self.total_clk_cycles),
-            bytes: self.buffer.drain(0..packet_len).collect()
+            bytes: self.buffer.drain(0..packet_len).collect(),
         })
     }
 
